1f43767b6457cb7a468496b44d0e66e5da7f929e
Timm Heuss <heussd@users.noreply.github.com>
2017-09-11 18:14:59 +0200

    Initial commit

---
+++ better-wikipedia-toc.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Better Wikipedia TOC
+// @author      T.H.
+// @namespace   https://github.com/heussd/better-wikipedia-toc
+// @include     *wiki*
+// @grant       GM_addStyle
+// ==/UserScript==
+
+// A product of the bday-h4ck 2017
+
+function addStyle(thatNewStyleCss) {
+  var thatNewStyle = document.createElement('style');
+  thatNewStyle.type = 'text/css';
+  thatNewStyle.innerHTML = thatNewStyleCss;
+  document.documentElement.appendChild(thatNewStyle);
+}
+
+addStyle('.toc {position: absolute; top: 70px; left: -177px; width: 160px; font-size: x-small !important;}');
+addStyle('div#mw-panel {z-index: -1;}');
+addStyle('.mw-content-ltr .toc ul ul, .mw-content-ltr #toc ul ul, .mw-content-rtl .mw-content-ltr .toc ul ul, .mw-content-rtl .mw-content-ltr #toc ul ul {margin: 0 0 0 0.2em}')
